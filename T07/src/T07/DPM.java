86f67142ed3174510d379ba3037224b01ebc558d
Simon Liu <ysimonliu@gmail.com>
2012-11-04 16:12:12 -0500

    DPM class ready to test the bluetooth RConsole

--- T07/src/T07/DPM.java
+++ T07/src/T07/DPM.java
@@ -1,5 +1,23 @@
 package T07;
 
+import lejos.nxt.Button;
+import lejos.nxt.comm.RConsole;
+
 public class DPM {
 	
+	public static void main(String[] args){
+		// This code now is to test the RConsole
+		RConsole.openBluetooth(20000);
+		RConsole.println("Connected!");
+		printNums();
+		
+		while (Button.readButtons() != Button.ID_ESCAPE);
+	}
+	
+	private static void printNums() {
+		int i;
+		for (i = 0; i < 10; i++) {
+			RConsole.println("The value of i is " + String.valueOf(i));
+		}
+	}
 }
